57805f9f7633e9865a892ecb95f3669a0ce25339
Carl B <c3jb@sbcglobal.net>
2021-08-05 15:32:20 -0500

    Streamlined button operation, made forms more uniform

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,19 +1,23 @@
-var button = document.getElementById("add");
-var input = document.getElementById("ingredientInput");
-var ul = document.getElementById("ingredients-list");
-var hiddenIngredients = document.getElementById("hide");
-
+const button = document.getElementById("add");
+const input = document.getElementById("ingredientInput");
+const label = document.getElementById("ingredientsLabel");
+const hiddenIngredients = document.getElementById("hiddenIngredients");
+const ul =document.createElement("ul");
 
 //This function adds the ingredients to a list to be saved to the recipe
 button.addEventListener("click", function() {
-  var li = document.createElement("li");
-  // Add Bootstrap class to the list element
-  li.classList.add("list-group-item");
-  //li.setAttribute("name","ingredients");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  hiddenIngredients.value += input.value + "$$";
-  // Clear your input
-  input.value = "";
+    // create list and add attributes
+    const li = document.createElement("li");
+    ul.classList.add("list-group");
+    ul.id = "ingredients-list";
+    li.classList.add("list-group-item");
+    li.setAttribute("name",input.value.toLowerCase());
+    // add list to document
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    label.appendChild(ul);
+    // assign ingredients to recipe and clear input field
+    hiddenIngredients.value += input.value + ", ";
+    input.value = "";
 })
 
