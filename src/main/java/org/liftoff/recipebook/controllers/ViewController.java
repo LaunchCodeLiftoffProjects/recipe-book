195885d280011885bbaf654fe0021b0e2fa897bf
Carl B <c3jb@sbcglobal.net>
2021-09-16 12:46:34 -0500

    allowed recipes to be displayed individually and showcase ids in url

--- src/main/java/org/liftoff/recipebook/controllers/ViewController.java
+++ src/main/java/org/liftoff/recipebook/controllers/ViewController.java
@@ -8,6 +8,8 @@ import org.liftoff.recipebook.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,31 +21,37 @@ import java.util.List;
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping(value = "view")
+@RequestMapping("view")
 public class ViewController {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
     @Autowired
     private RecipeCategoryRepository recipeCategoryRepository;
+
     @Autowired
     AuthenticationController authenticationController;
+
     @Autowired
     private UserRepository userRepository;
 
 
-    @RequestMapping
-    public String view(Model model, HttpServletRequest request) {
+    @RequestMapping("/{recipeId}")
+    public String view(Model model, @PathVariable int recipeId, HttpServletRequest request) {
         HttpSession session = request.getSession();
         User sessionUser = authenticationController.getUserFromSession(session);
         int userId = sessionUser.getId();
+        User user = userRepository.findById(userId).get();
 
+        model.addAttribute("recipe", recipeRepository.findById(recipeId));
+        model.addAttribute("profile", userRepository.findById(userId).get());
         model.addAttribute("recipes", recipeRepository.findAll());
         model.addAttribute("categories", recipeCategoryRepository.findAll());
-        model.addAttribute("profile", userRepository.findById(userId).get());
+        model.addAttribute("user", user);
 //        List<String> ingredientList = Arrays.asList(recipe.ingredients.split("$$"));
 
-        return "view/{id}";
+        return "view";
     }
 
 }
